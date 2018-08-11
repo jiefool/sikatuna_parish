be11cc0876fb6066abc451d8303a9da732d07c8a
Jay Paul Aying <jaypaulaying5@gmail.com>
2018-08-11 08:51:40 +0800

    views

--- app/src/main/java/com/bagollabs/sikatuna_parish/myapplication/MainActivity.java
+++ app/src/main/java/com/bagollabs/sikatuna_parish/myapplication/MainActivity.java
@@ -5,6 +5,9 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.CalendarView;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,15 +19,19 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            FrameLayout contentLayout = (FrameLayout) findViewById(R.id.content);
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    CalendarView calendarView = (CalendarView) findViewById(R.id.calendar_view);
+                    contentLayout.addView(calendarView);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    LinearLayout eventView = (LinearLayout) findViewById(R.id.events_view);
+                    contentLayout.addView(eventView);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+
                     return true;
             }
             return false;
@@ -37,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+//        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
