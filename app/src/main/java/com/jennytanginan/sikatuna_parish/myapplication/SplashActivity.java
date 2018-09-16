51c617dec4ab38ba9701c2ad873f18d28eb43888
jennytanginan44 <jennytanginan44@gmail.com>
2018-09-16 13:11:45 +0800

    some comment

---
+++ app/src/main/java/com/jennytanginan/sikatuna_parish/myapplication/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.jennytanginan.sikatuna_parish.myapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.splash);
+
+        Thread timer = new Thread() {
+            public void run() {
+                try {
+                    sleep(1000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent openClass = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(openClass);
+                }
+            }
+        };
+
+        timer.start();
+
+    }
+}
+
